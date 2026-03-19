476eca6bf439cccb30872c3b5682dc3cf4d76812
keerthi-jk <137547561+keerthi-jk@users.noreply.github.com>
2026-03-20 01:27:22 +0530

    Update App.java
    Fixed a moderate issue from sonar qube

--- src/main/java/com/example/demo/App.java
+++ src/main/java/com/example/demo/App.java
@@ -1,6 +1,9 @@
 package com.example.demo;
 
 public class App {
+     private App() {
+        // prevent instantiation
+    }
     public static int add(int a, int b) {
         return a + b;
     }
